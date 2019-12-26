857b40ab776a0bd92de55651c1419e57d927c205
Aakash Pal <Aakash@cloudthing.com>
2019-12-26 20:54:20 +0530

    Updates

---
+++ precache-manifest.c74e6c0e502c007c3547d5b09c51fea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1181857a649000dfe00459256939cbe",
+    "url": "/user-app/index.html"
+  },
+  {
+    "revision": "ea3ffb52eba23cc042fb",
+    "url": "/user-app/static/css/main.32cb3eae.chunk.css"
+  },
+  {
+    "revision": "e0e60f5d9648da431069",
+    "url": "/user-app/static/js/2.3922cf23.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/user-app/static/js/2.3922cf23.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea3ffb52eba23cc042fb",
+    "url": "/user-app/static/js/main.40f0a51c.chunk.js"
+  },
+  {
+    "revision": "8f1d44d8291dbfb21513",
+    "url": "/user-app/static/js/runtime-main.07389be6.js"
+  }
+]);
