7f8e31fbdbf1d793dbe1fe4ba522cc98f0ebfbdb
Aakash Pal <Aakash@cloudthing.com>
2019-12-26 21:19:40 +0530

    fix div align issues

--- src/UserCard.js
+++ src/UserCard.js
@@ -15,21 +15,23 @@ function UserCard() {
       {!user && <h2 style={{ textAlign: "center" }}>Loading profile</h2>}
       {user && (
         <div style={profileStyle}>
-          <div style={{ width: "500px", height: "500px", float: "left" }}>
+          <div style={{ width: "30%", height: "80%", float: "left" }}>
             <img
               src="https://images.fineartamerica.com/images-medium-large-5/businessman-vector-icon-art-sonik.jpg"
               alt="Profile"
               style={{
-                width: "500px",
-                height: "500px",
+                padding: "20px",
+                width: "75%",
+                height: "75%",
                 borderRadius: "50%"
               }}
             ></img>
           </div>
           <div
             style={{
-              width: "900px",
-              height: "500px",
+              padding: "30px",
+              width: "70%",
+              height: "80%",
               float: "right",
               fontSize: 30,
               color: "white"
@@ -38,11 +40,12 @@ function UserCard() {
             <p>Name: {user.name}</p>
             <p>Username: {user.username}</p>
             <p>Email: {user.email}</p>
+            <p>Phone: {user.phone}</p>
             <p>
               Address: {user.address.street}, {user.address.suite},{" "}
-              {user.address.city}, {user.address.zipcode}
+              {user.address.city}
             </p>
-            <p>Phone: {user.phone}</p>
+            <p>Zipcode: {user.address.zipcode}</p>
             <p>
               Website: <u>{user.website}</u>
             </p>
