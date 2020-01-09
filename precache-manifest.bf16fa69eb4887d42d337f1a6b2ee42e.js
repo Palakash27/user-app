42cc1353b8ba7ca018ffc3d75f753814a5cc0eff
Aakash Pal <Aakash@cloudthing.com>
2020-01-09 11:51:05 +0530

    Updates

--- precache-manifest.bf16fa69eb4887d42d337f1a6b2ee42e.js
+++ precache-manifest.bf16fa69eb4887d42d337f1a6b2ee42e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c0c4843b1beeb8a4c35cee9e1659e214",
+    "revision": "a2ca0700ddd04636cf22fb35509252f9",
     "url": "/user-app/index.html"
   },
   {
-    "revision": "775a80cb6a49080684a4",
+    "revision": "3ec6e37fa2f2e51b11fb",
     "url": "/user-app/static/css/main.32cb3eae.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/user-app/static/js/2.3922cf23.chunk.js.LICENSE"
   },
   {
-    "revision": "775a80cb6a49080684a4",
-    "url": "/user-app/static/js/main.bb838037.chunk.js"
+    "revision": "3ec6e37fa2f2e51b11fb",
+    "url": "/user-app/static/js/main.0a5aea48.chunk.js"
   },
   {
     "revision": "8f1d44d8291dbfb21513",
